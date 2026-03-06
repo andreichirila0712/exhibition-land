0dcf4c3ec02951e4eb9642f2e127101d0d53f810
”Andrei” <andrei.chirila07121999@gmail.com>
2026-03-06 15:52:51 +0200

    feat: add unit tests for presentation service

---
+++ src/test/java/andrei/chirila/prove_yourself/ProveYourselfApplicationTests.java
@@ -0,0 +1,13 @@
+package andrei.chirila.prove_yourself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveYourselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/andrei/chirila/prove_yourself/presentation/PresentationTest.java
@@ -0,0 +1,83 @@
+package andrei.chirila.prove_yourself.presentation;
+
+import andrei.chirila.prove_yourself.user.User;
+import andrei.chirila.prove_yourself.user.UserService;
+import andrei.chirila.prove_yourself.utils.S3Utility;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class PresentationTest {
+    private static final String USER_ID = "user123";
+
+    @Mock
+    private S3Utility s3Utility;
+    @Mock
+    private PresentationRepository presentationRepository;
+    @Mock
+    private UserService userService;
+    @InjectMocks
+    private PresentationService presentationService;
+    @Captor
+    private ArgumentCaptor<Presentation> presentationCaptor;
+
+    @Test
+    public void postPresentation() {
+        PresentationDTO presentationDTO = new PresentationDTO(
+                "Test Presentation",
+                "https://test.com",
+                "Test Description"
+        );
+
+        Mockito.when(s3Utility.uploadFile(any(), any(), any())).thenReturn("thumbnail-user123.jpg");
+        Mockito.when(presentationRepository.save(any())).thenReturn(new Presentation("Test Presentation", "thumbnail-user123.jpg", "https://test.com", "Test Description", new User(USER_ID, "u1", "u1", "u1", "u1")));
+
+        Presentation savedPresentation = presentationService.savePresentation(USER_ID, presentationDTO, null);
+
+        assertEquals("Test Presentation", savedPresentation.getName());
+        assertEquals("Test Description", savedPresentation.getDescription());
+        assertEquals("https://test.com", savedPresentation.getLink());
+        assertEquals("thumbnail-user123.jpg", savedPresentation.getThumbnail());
+    }
+
+    @Test
+    public void getPresentation() {
+        Presentation presentation = new Presentation("Test Presentation", "thumbnail-user123.jpg", "https://test.com", "Test Description", new User(USER_ID, "u1", "u1", "u1", "u1"));
+        Mockito.when(presentationRepository.findPresentationByUserIdAndName(any(), any())).thenReturn(Optional.of(presentation));
+        Presentation retrievedPresentation = presentationService.retrievePresentation(USER_ID, "Test Presentation");
+
+        assertEquals(presentation, retrievedPresentation);
+    }
+
+    @Test
+    public void deletePresentation() {
+        presentationService.deletePresentation(USER_ID, "Test Presentation");
+        Mockito.verify(presentationRepository).deletePresentationByUserIdAndName(any(), any());
+    }
+
+    @Test
+    public void patchPresentation() {
+        Presentation presentation = new Presentation("Test Presentation", "thumbnail-user123.jpg", "https://test.com", "Test Description", new User(USER_ID, "u1", "u1", "u1", "u1"));
+        Mockito.when(presentationRepository.findPresentationByUserIdAndName(any(), any())).thenReturn(Optional.of(presentation));
+        Map<String, String> data = Map.of("name", "New Name", "description", "New Description", "link", "https://new.com");
+        presentationService.patchPresentation(USER_ID, "Test Presentation", data);
+        Mockito.verify(presentationRepository).save(presentationCaptor.capture());
+
+        assertEquals("New Name", presentationCaptor.getValue().getName());
+        assertEquals("New Description", presentationCaptor.getValue().getDescription());
+        assertEquals("https://new.com", presentationCaptor.getValue().getLink());
+    }
+
+}
