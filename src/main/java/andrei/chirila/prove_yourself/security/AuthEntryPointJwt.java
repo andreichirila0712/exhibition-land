160a55d6304acab70fc0707ba271aaca4c4ba984
”Andrei” <andrei.chirila07121999@gmail.com>
2026-03-12 19:09:07 +0200

    feat: add web security configuration
    * Added web security configuration;
    * Added missing component annotation on JWT auth entry point.

--- src/main/java/andrei/chirila/prove_yourself/security/AuthEntryPointJwt.java
+++ src/main/java/andrei/chirila/prove_yourself/security/AuthEntryPointJwt.java
@@ -1,13 +1,14 @@
 package andrei.chirila.prove_yourself.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     @Override
