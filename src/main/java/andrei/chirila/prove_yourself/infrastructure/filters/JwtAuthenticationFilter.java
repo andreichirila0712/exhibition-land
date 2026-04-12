e7a6d748b5da804c49bef383e8d7f67cf6fbf6ed
”Andrei” <andrei.chirila07121999@gmail.com>
2026-04-13 00:50:55 +0300

    fix: solve issue with jwt filter
    * Solved a misconfiguration when it comes to filtering the requests. Before it was configured wrongly with a LogoutFilter instead of the custom JwtFilter and username-password authentication filter;
    * Dropped the initialization of the custom filter inside the web configuration class, now is properly injected by Spring (added the filter as a param for the filter chain method and made the filter (class) a component);
    * Configured properly the logout. Went for a ResponseCookie (Spring based) instead of a default Cookie (Jakarta) and set the header accordingly;
    * Implemented the doFilterInternal method to properly filter and authorize requests. In case of failing to validate the token (from cookie) it will set a default status of "unauthorized" and then the filtering stops;

--- src/main/java/andrei/chirila/prove_yourself/infrastructure/filters/JwtAuthenticationFilter.java
+++ src/main/java/andrei/chirila/prove_yourself/infrastructure/filters/JwtAuthenticationFilter.java
@@ -11,7 +11,11 @@ import jakarta.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.thymeleaf.util.ArrayUtils;
 
@@ -20,6 +24,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
     private static final List<String> URL_MATCHERS = List.of(WebSecurityConfig.WELCOME_URL_MATCHER, WebSecurityConfig.LOGIN_URL_MATCHER, WebSecurityConfig.REGISTRATION_URL_MATCHER, WebSecurityConfig.CREATE_USER_URL_MATCHER, WebSecurityConfig.ACCOUNT_VERIFICATION_URL_MATCHER, "/css/prove.css");
@@ -36,21 +41,42 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         final Optional<String> token = getJwtFromCookie(request);
+
+        if (token.isEmpty()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (!authService.validateToken(token.get())) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            logger.warn("[AUTH] : Token is invalid for request: {}", request.getRequestURI());
+            return;
+        }
+
+        String userName = authService.getUserFromToken(token.get());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        authenticationToken.setDetails(userDetails);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        logger.info("[AUTH] : Request successfully authorized for user: {}", userName);
+
+        filterChain.doFilter(request, response);
     }
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         final String requestURI = request.getRequestURI();
-
         return URL_MATCHERS.contains(requestURI);
     }
 
     private Optional<String> getJwtFromCookie(HttpServletRequest request) {
         final Cookie[] cookies = request.getCookies();
+
         if (cookies == null || ArrayUtils.isEmpty(cookies)) {
             return Optional.empty();
         }
-        
+
         return (Arrays.stream(cookies)
                 .filter(cookie -> cookie.getName().equals(cookieName))
                 .map(Cookie::getValue)
