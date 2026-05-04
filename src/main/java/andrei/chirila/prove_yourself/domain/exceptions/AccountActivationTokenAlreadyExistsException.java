e786f37b7828c2bc0da74aaf756641a7104d948e
”Andrei” <andrei.chirila07121999@gmail.com>
2026-05-05 00:37:11 +0300

    refactor: remove unused custom exceptions

--- src/main/java/andrei/chirila/prove_yourself/domain/exceptions/AccountActivationTokenAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package andrei.chirila.prove_yourself.domain.exceptions;
-
-public class AccountActivationTokenAlreadyExistsException extends RuntimeException {
-    public AccountActivationTokenAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/andrei/chirila/prove_yourself/domain/exceptions/UserAlreadyVerifiedException.java
+++
@@ -1,7 +0,0 @@
-package andrei.chirila.prove_yourself.domain.exceptions;
-
-public class UserAlreadyVerifiedException extends RuntimeException {
-    public UserAlreadyVerifiedException(String message) {
-        super(message);
-    }
-}
