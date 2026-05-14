1f3e52b48371bd2c4410b2ee94c83ef567e5b5ee
”Andrei” <andrei.chirila07121999@gmail.com>
2026-05-14 22:41:09 +0300

    feat: add support for new custom exceptions
    * Declared new custom exceptions for different cases (username being sent empty, too many characters for about, empty name, invalid website url);
    * Handled those new custom exceptions and set specific response statuses and messages.

--- src/main/java/andrei/chirila/prove_yourself/domain/exceptions/ElErrorMessage.java
+++ src/main/java/andrei/chirila/prove_yourself/domain/exceptions/ElErrorMessage.java
@@ -11,6 +11,15 @@ public enum ElErrorMessage {
     USER_NOT_VERIFIED(HttpStatus.INTERNAL_SERVER_ERROR, "User not verified"),
     //EMAIL
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "Email already exists"),
+    //USERNAME
+    USERNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "Username already exists"),
+    USERNAME_IS_EMPTY(HttpStatus.BAD_REQUEST, "Username cannot be empty"),
+    //NAME
+    NAME_IS_EMPTY(HttpStatus.BAD_REQUEST, "Name cannot be empty"),
+    //ABOUT
+    ABOUT_IS_TOO_LENGTHY(HttpStatus.BAD_REQUEST, "About should not be longer than 500 characters"),
+    //WEBSITE
+    URL_DOES_NOT_MATCH_PATTERN(HttpStatus.BAD_REQUEST, "URL must have a valid domain"),
     //ACCOUNT ACTIVATION TOKEN
     ACCOUNT_ACTIVATION_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Account activation token not found"),
     ACCOUNT_ACTIVATION_TOKEN_ALREADY_EXISTS(HttpStatus.CONFLICT, "Account activation token already exists"),
@@ -18,12 +27,10 @@ public enum ElErrorMessage {
     EMAIL_CHANGE_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Email change token not found"),
     EMAIL_CHANGE_TOKEN_ALREADY_EXISTS(HttpStatus.CONFLICT, "Email change token already exists"),
     EMAIL_CHANGE_TOKEN_IS_EXPIRED(HttpStatus.GONE, "Email change token is expired"),
-    EMAIL_CHANGE_TOKEN_NO_EMAIL_ASSOCIATED(HttpStatus.INTERNAL_SERVER_ERROR, "Email change token hs no email associated"),
     //PASSWORD CHANGE TOKEN
     PASSWORD_CHANGE_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Password change token not found"),
     PASSWORD_CHANGE_TOKEN_ALREADY_EXISTS(HttpStatus.CONFLICT, "Password change token already exists"),
     PASSWORD_CHANGE_TOKEN_IS_EXPIRED(HttpStatus.GONE, "Password change token is expired"),
-    PASSWORD_CHANGE_TOKEN_NO_USER_ASSOCIATED(HttpStatus.INTERNAL_SERVER_ERROR, "Password change token hs no user associated"),
     //GENERIC
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong"),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "Something went wrong"),
